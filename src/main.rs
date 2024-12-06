d10b62f17ca3e9a366956fc568d8d51d3685aefb
edpyt <edrsa1321s@gmail.com>
2024-12-07 00:17:46 +0500

    refactor code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
+use zero2prod::{configuration::get_configuration, run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let configuraton = get_configuration().expect("Failed to read configuration.");
     let listener = TcpListener::bind("127.0.0.1:8000")?;
     run(listener)?.await
 }
