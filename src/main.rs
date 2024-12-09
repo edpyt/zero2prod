353c5ff5622de94756f37d4488db0296256f4231
edpyt <edrsa1321s@gmail.com>
2024-12-09 09:22:12 +0500

    add logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -5,11 +6,16 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuraton = get_configuration().expect("Failed to read configuration.");
+
     let connection_pool = PgPool::connect(&configuraton.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
+
     let address = format!("127.0.0.1:{}", configuraton.application_port);
     let listener = TcpListener::bind(address)?;
+
     run(listener, connection_pool)?.await
 }
