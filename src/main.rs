09e5abbad37a4a476695785e83aaf093e274f52e
edpyt <edrsa1321s@gmail.com>
2024-12-12 15:59:52 +0300

    refactor: rename variables
    probably i was too sleepy lol :D

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,13 @@ use zero2prod::startup::run;
 async fn main() -> Result<(), std::io::Error> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
-    let configuraton = get_configuration().expect("Failed to read configuration.");
+    let configuration = get_configuration().expect("Failed to read configuration.");
 
-    let connection_pool = PgPool::connect(&configuraton.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
 
-    let address = format!("127.0.0.1:{}", configuraton.application_port);
+    let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
 
     run(listener, connection_pool)?.await
