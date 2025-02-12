1c9e66009982ca68a6ba994377626a1649259dd0
edpyt <edrsa1321s@gmail.com>
2025-02-12 09:25:50 +0500

    chore(flash): use actix-web-flash-messages

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,9 @@
+use actix_web::cookie::Key;
 use actix_web::web::Data;
 use actix_web::{dev::Server, web, App, HttpServer};
-use secrecy::Secret;
+use actix_web_flash_messages::storage::CookieMessageStore;
+use actix_web_flash_messages::FlashMessagesFramework;
+use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
@@ -65,9 +68,14 @@ pub fn run(
     let db_pool = Data::new(db_pool);
     let email_client = Data::new(email_client);
     let base_url = Data::new(ApplicationBaseUrl(base_url));
-    //
+    // middleware
+    let message_store =
+        CookieMessageStore::builder(Key::from(hmac_secret.expose_secret().as_bytes())).build();
+    let message_framework = FlashMessagesFramework::builder(message_store).build();
+
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(message_framework.clone())
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
