621ebfb39d87807d7eb57c7fa48cc7bf35f136ec
edpyt <edrsa1321s@gmail.com>
2025-02-27 09:03:07 +0500

    WIP

--- src/routes/admin/newsletter/post.rs
+++ src/routes/admin/newsletter/post.rs
@@ -10,7 +10,8 @@ use crate::{
     authentication::UserId,
     domain::SubscriberEmail,
     email_client::EmailClient,
-    utils::{e500, see_other},
+    idempotency::IdempotencyKey,
+    utils::{e400, e500, see_other},
 };
 
 #[derive(serde::Deserialize)]
@@ -18,6 +19,7 @@ pub struct FormData {
     title: String,
     text_content: String,
     html_content: String,
+    idempotency_key: String,
 }
 
 #[
@@ -33,17 +35,19 @@ pub async fn publish_newsletter(
     pool: web::Data<PgPool>,
     email_client: web::Data<EmailClient>,
 ) -> Result<HttpResponse, actix_web::Error> {
+    let FormData {
+        title,
+        text_content,
+        html_content,
+        idempotency_key,
+    } = form.0;
+    let idempotency_key: IdempotencyKey = idempotency_key.try_into().map_err(e400)?;
     let subscribers = get_confirmed_subscribers(&pool).await.map_err(e500)?;
     for subscriber in subscribers {
         match subscriber {
             Ok(subscriber) => {
                 email_client
-                    .send_email(
-                        &subscriber.email,
-                        &form.title,
-                        &form.html_content,
-                        &form.text_content,
-                    )
+                    .send_email(&subscriber.email, &title, &html_content, &text_content)
                     .await
                     .with_context(|| {
                         format!("Failed to send newsltter issue to {}", subscriber.email)
--- tests/api/main.rs
+++ tests/api/main.rs
@@ -3,5 +3,6 @@ mod change_password;
 mod health_check;
 mod helpers;
 mod login;
+mod newsletetter;
 mod subscriptions;
 mod subscriptions_confirm;
